2f4026877c969fd63f67e7d91f4044f499c03e6b
unknown <snaved2004@gmail.com>
2024-04-04 01:57:43 +0530

    block added

--- source/index.js
+++ source/index.js
@@ -9,7 +9,7 @@ require("./database/conn");
 
 app.use(cors({origin: "*"}));
 
-app.use(fileUpload()); 
+app.use(fileUpload());
 
 app.use("/shop", express.static(path.join(__dirname, 'uploads')));
 
@@ -20,6 +20,16 @@ app.use(express.json());
 
 app.use("/shop", require("./endpoints/endpoints"))
 
-app.listen(port,()=>{
-   console.log("hello express")
-})
+const start = async()=>{
+   try {
+      
+      app.listen(port,()=>{
+         console.log("hello express")
+      })
+
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+start();
